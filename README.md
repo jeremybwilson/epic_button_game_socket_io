e335756ce2d3b98fe8efc47aa53fac20b5978a0a
Jeremy Wilson <jeremybwilson@gmail.com>
2018-10-31 12:22:25 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Coding Dojo Bootcamp Assignment
+## MEAN, JavaScript, Express JS, Socket.io / Socket.io / Epic Button Game Assignment
+
+### Repository Link
+
+> ``` https://github.com/jeremybwilson/hello_express.git ```<br>
+> _[Click Here](https://github.com/jeremybwilson/hello_express.git)_
---
+++ server.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const session = require('express-session');
+const parser = require('body-parser');
+const path = require('path');
+
+const port = process.env.PORT || 8002;
+// invoke express and store the result in the variable app
+const app = express();
+
+app.use(parser.urlencoded({ extended: true }));
+app.use(parser.json());
+
+app.use(express.static(path.join(__dirname, 'static')));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.get('/', (request, response) => {
+    console.log('getting to index');
+    response.render('index', { number, title: 'Epic Number Game' });
+});
+
+// const server = app.listen(1337);
+const server = app.listen(port, () => console.log(`Express server listening on port ${port}`));    // ES6 way
+const io = require('socket.io')(server);
+
+let count = 0;
+const resetCount = 0;
+
+io.on('connection', (socket) => {
+    console.log('incoming socket connection');
+
+    socket.on('buttonClicked', () => {
+        numberUpdated(++count);
+    });
+
+    socket.on('resetClicked', (socket) => {
+        numberUpdated(resetCount);
+    });
+
+    socket.emit('numberUpdated', count);
+});
+
+function numberUpdated(number) {
+    io.emit('numberUpdated', number);
+};
+
+// catch 404 and forward to error handler
+app.use((request, response, next) => {
+    const err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handler
+app.use((err, request, response, next) => {
+    // set locals, only providing error in development
+    response.locals.message = err.message;
+    response.locals.error = request.app.get('env') === 'development' ? err : {};
+    response.status(err.status || 500);
+    // render the error page
+    response.render('error', {title: 'Error page'});
+  });
+
+// app.listen(port, () => console.log(`Express server listening on port ${port}`));    // ES6 way
